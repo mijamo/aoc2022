ff7a520dc72fd9495dafeb27bf435a7a09ffeca2
Mikael Gourlaouen <gourlaouen.mikael@gmail.com>
2022-12-20 16:10:54 +0100

    Solve day 6 part 2

--- aoc2022/crates/dec6/src/main.rs
+++ aoc2022/crates/dec6/src/main.rs
@@ -6,7 +6,7 @@ fn main() -> std::io::Result<()> {
     let file = File::open("./src/input.txt")?;
     let reader = BufReader::new(file);
     let content = reader.lines().next().unwrap().unwrap();
-    let mut start_buffer = ['z', 'z', 'z', 'z'];
+    let mut start_buffer: [char; 14] = ['z'].repeat(14).try_into().unwrap();
     for (i, c) in content.char_indices() {
         start_buffer = start_buffer[1..]
             .into_iter()
@@ -15,9 +15,9 @@ fn main() -> std::io::Result<()> {
             .collect::<Vec<char>>()
             .try_into()
             .unwrap();
-        if HashSet::<char>::from_iter(start_buffer).len() == 4 {
+        if HashSet::<char>::from_iter(start_buffer).len() == 14 {
             println!(
-                "The first 4 characters are {} and can be found starting from position {}",
+                "The first 14 characters are {} and can be found finishing at position {}",
                 start_buffer.into_iter().collect::<String>(),
                 i + 1
             );
