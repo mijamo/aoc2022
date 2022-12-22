07fc0dfde08f0f3bc5a0f87b0bd15d7c29c2ac14
Mikael Gourlaouen <gourlaouen.mikael@gmail.com>
2022-12-22 23:46:09 +0100

    Day 13 part 2

--- aoc2022/crates/dec13/src/main.rs
+++ aoc2022/crates/dec13/src/main.rs
@@ -6,6 +6,7 @@ use nom::multi::{many0, many1, separated_list0};
 use nom::sequence::{delimited, tuple};
 use nom::IResult;
 use std::cmp::Ordering;
+use std::cmp::PartialOrd;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Read};
 
@@ -55,6 +56,24 @@ impl PacketData {
     }
 }
 
+impl PartialEq for PacketData {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Value(lhs), Self::Value(rhs)) => lhs == rhs,
+            (Self::List(_), Self::Value(_)) => false,
+            (Self::Value(_), Self::List(_)) => false,
+            (Self::List(lhs), Self::List(rhs)) => {
+                if lhs.len() != rhs.len() {
+                    return false;
+                }
+                lhs.iter()
+                    .zip(rhs.iter())
+                    .all(|(lhs_v, rhs_v)| lhs_v == rhs_v)
+            }
+        }
+    }
+}
+
 fn number(input: &str) -> IResult<&str, PacketData> {
     let (input, number) = map_res(digit1, str::parse)(input)?;
     Ok((input, PacketData::Value(number)))
@@ -76,25 +95,42 @@ fn pair(input: &str) -> IResult<&str, (PacketData, PacketData)> {
     Ok((input, (first, second)))
 }
 
-fn signal(input: &str) -> IResult<&str, Vec<(PacketData, PacketData)>> {
-    separated_list0(many1(line_ending), pair)(input)
+fn signal(input: &str) -> IResult<&str, Vec<PacketData>> {
+    separated_list0(many0(line_ending), array)(input)
 }
 
 fn main() -> std::io::Result<()> {
     let mut file = File::open("./src/input.txt")?;
     let mut content = String::new();
     file.read_to_string(&mut content)?;
-    let (_, signal) = signal(&content).unwrap();
-    println!("Parsed {} pairs", signal.len());
-    let index_sum: usize = signal
+    let (_, mut signal) = signal(&content).unwrap();
+    let (_, first_divider) = array("[[2]]").unwrap();
+    let (_, second_divider) = array("[[6]]").unwrap();
+    println!("Parsed {} packets", signal.len());
+    signal.push(first_divider);
+    signal.push(second_divider);
+    signal.sort_unstable_by(|a, b| match a.comes_before(b) {
+        CmpRes::Yes => Ordering::Less,
+        CmpRes::No => Ordering::Greater,
+        CmpRes::Maybe => Ordering::Equal,
+    });
+
+    let (_, first_divider) = array("[[2]]").unwrap();
+    let (_, second_divider) = array("[[6]]").unwrap();
+    let first_index = signal
         .iter()
         .enumerate()
-        .filter_map(|(i, (first, second))| match first.comes_before(second) {
-            CmpRes::Yes => Some(i + 1),
-            CmpRes::No => None,
-            CmpRes::Maybe => panic!("Unexpected comparison result"),
-        })
-        .sum();
-    println!("Indexes of packets in right order sum to {}", index_sum);
+        .find(|(_, p)| *p == &first_divider)
+        .unwrap()
+        .0
+        + 1;
+    let second_index = signal
+        .iter()
+        .enumerate()
+        .find(|(_, p)| *p == &second_divider)
+        .unwrap()
+        .0
+        + 1;
+    println!("Decoder key is {}", first_index * second_index);
     Ok(())
 }
