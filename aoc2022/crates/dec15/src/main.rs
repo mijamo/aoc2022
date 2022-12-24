f96ae2472c0ac929fedaa1e45033cb30a07780aa
Mikael Gourlaouen <gourlaouen.mikael@gmail.com>
2022-12-24 17:56:11 +0100

    Solved dec 15

--- aoc2022/crates/dec15/src/main.rs
+++ aoc2022/crates/dec15/src/main.rs
@@ -2,7 +2,7 @@ use nom::bytes::complete::tag;
 use nom::character::complete::line_ending;
 use nom::multi::separated_list1;
 use nom::{character::complete::i32 as number, sequence::preceded, IResult};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::Read;
 
@@ -16,62 +16,46 @@ impl Pos {
     fn distance(&self, other: &Pos) -> u32 {
         i32::abs_diff(self.x, other.x) + i32::abs_diff(self.y, other.y)
     }
+}
 
-    fn around(&self, radius: u32) -> Vec<Pos> {
-        let radius = radius as i32;
-        let mut points_around: Vec<Pos> = Vec::new();
-        for d_x in 0..radius + 1 {
-            for d_y in 0..radius + 1 - d_x {
-                points_around.push(Pos {
-                    x: self.x + d_x,
-                    y: self.y + d_y,
-                });
-                points_around.push(Pos {
-                    x: self.x - d_x,
-                    y: self.y - d_y,
-                });
-                points_around.push(Pos {
-                    x: self.x + d_x,
-                    y: self.y - d_y,
-                });
-                points_around.push(Pos {
-                    x: self.x - d_x,
-                    y: self.y + d_y,
-                });
-            }
-        }
-        points_around
+struct Pair {
+    sensor: Pos,
+    beacon: Pos,
+}
+
+impl Pair {
+    fn radius(&self) -> u32 {
+        self.sensor.distance(&self.beacon)
     }
 }
 
-enum Presence {
-    Impossible,
-    Beacon,
-    Sensor,
-}
-
 struct Arena {
-    layout: HashMap<Pos, Presence>,
+    layout: Vec<Pair>,
 }
 
 impl Arena {
-    fn new(input: Vec<(Pos, Pos)>) -> Self {
-        let mut layout: HashMap<Pos, Presence> = HashMap::new();
-        for (signal, beacon) in input.into_iter() {
-            layout.insert(signal, Presence::Sensor);
-            layout.insert(beacon, Presence::Beacon);
-            let radius = signal.distance(&beacon);
-            for pos in signal.around(radius).iter() {
-                match layout.get(pos) {
-                    None => {
-                        layout.insert(*pos, Presence::Impossible);
-                    }
-                    Some(_) => {}
-                }
+    fn new(layout: Vec<Pair>) -> Self {
+        Self { layout }
+    }
+
+    fn rule_out_at(&self, line: i32) -> usize {
+        let mut ruled_out: HashSet<i32> = HashSet::new();
+        let mut beacons_on_line: HashSet<i32> = HashSet::new();
+        for pair in self.layout.iter() {
+            let d_y = i32::abs_diff(pair.sensor.y, line);
+            let radius = pair.radius();
+            if pair.beacon.y == line {
+                beacons_on_line.insert(pair.beacon.x);
+            }
+            if d_y > radius {
+                continue;
+            }
+            for d_x in 0..radius + 1 - d_y {
+                ruled_out.insert(pair.sensor.x + d_x as i32);
+                ruled_out.insert(pair.sensor.x - d_x as i32);
             }
         }
-        println!("GENERATED");
-        Self { layout }
+        ruled_out.difference(&beacons_on_line).count()
     }
 }
 
@@ -81,14 +65,14 @@ fn pos(input: &str) -> IResult<&str, Pos> {
     Ok((input, Pos { x, y }))
 }
 
-fn couple(input: &str) -> IResult<&str, (Pos, Pos)> {
+fn pair(input: &str) -> IResult<&str, Pair> {
     let (input, sensor) = preceded(tag("Sensor at "), pos)(input)?;
     let (input, beacon) = preceded(tag(": closest beacon is at "), pos)(input)?;
-    Ok((input, (sensor, beacon)))
+    Ok((input, Pair { sensor, beacon }))
 }
 
-fn scan(input: &str) -> IResult<&str, Vec<(Pos, Pos)>> {
-    separated_list1(line_ending, couple)(input)
+fn scan(input: &str) -> IResult<&str, Vec<Pair>> {
+    separated_list1(line_ending, pair)(input)
 }
 
 fn main() -> std::io::Result<()> {
@@ -96,15 +80,9 @@ fn main() -> std::io::Result<()> {
     let mut content = String::new();
     file.read_to_string(&mut content).unwrap();
     let (_, data) = scan(&content).unwrap();
+    println!("{} pairs parsed", data.len());
     let arena = Arena::new(data);
-    let ruled_out = arena
-        .layout
-        .iter()
-        .filter(|(pos, cell)| match cell {
-            Presence::Beacon => false,
-            _ => pos.y == 2000000,
-        })
-        .count();
+    let ruled_out = arena.rule_out_at(2000000);
     println!("{} cells ruled out", ruled_out);
     Ok(())
 }
