6ce9898be9fff34c4a723d2c4d3fe662bb1367d7
Mikael Gourlaouen <gourlaouen.mikael@gmail.com>
2022-12-31 16:52:15 +0100

    Solved dec 22 part 2

--- aoc2022/crates/dec22/src/main.rs
+++ aoc2022/crates/dec22/src/main.rs
@@ -4,11 +4,12 @@ use nom::combinator::map;
 use nom::multi::{many1, separated_list1};
 use nom::sequence::separated_pair;
 use nom::IResult;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
-use std::ops::Add;
+use std::ops::{Add, Range};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct Pos {
     x: usize,
     y: usize,
@@ -51,10 +52,232 @@ enum Cell {
     Wall,
 }
 
+struct Bounds {
+    x: Range<usize>,
+    y: Range<usize>,
+}
+
+impl Bounds {
+    fn contains(&self, pos: &Pos) -> bool {
+        self.x.contains(&pos.x) && self.y.contains(&pos.y)
+    }
+}
+
+struct Face {
+    bounds: Bounds,
+}
+
+impl Face {
+    fn at(&self, position: usize, from_dir: &Direction, direction: &Direction) -> Pos {
+        let y = match (from_dir, direction) {
+            (_, Direction::Bottom) => self.bounds.y.start,
+            (_, Direction::Top) => self.bounds.y.end - 1,
+            (Direction::Left, Direction::Left)
+            | (Direction::Right, Direction::Right)
+            | (Direction::Top, Direction::Right)
+            | (Direction::Bottom, Direction::Left) => self.bounds.y.start + position,
+            (Direction::Bottom, Direction::Right)
+            | (Direction::Top, Direction::Left)
+            | (Direction::Right, Direction::Left)
+            | (Direction::Left, Direction::Right) => self.bounds.y.end - 1 - position,
+        };
+        let x = match (from_dir, direction) {
+            (_, Direction::Right) => self.bounds.x.start,
+            (_, Direction::Left) => self.bounds.x.end - 1,
+            (Direction::Top, Direction::Top)
+            | (Direction::Bottom, Direction::Bottom)
+            | (Direction::Left, Direction::Bottom)
+            | (Direction::Right, Direction::Top) => self.bounds.x.start + position,
+            (Direction::Bottom, Direction::Top)
+            | (Direction::Top, Direction::Bottom)
+            | (Direction::Right, Direction::Bottom)
+            | (Direction::Left, Direction::Top) => self.bounds.x.end - 1 - position,
+        };
+        Pos { x, y }
+    }
+}
+
+struct Cube {
+    faces: Vec<Face>,
+}
+
+impl Cube {
+    fn new_test() -> Self {
+        Self {
+            faces: Vec::from([
+                Face {
+                    bounds: Bounds {
+                        x: (9..13),
+                        y: (1..5),
+                    },
+                },
+                Face {
+                    bounds: Bounds {
+                        x: (1..5),
+                        y: (5..9),
+                    },
+                },
+                Face {
+                    bounds: Bounds {
+                        x: (5..9),
+                        y: (5..9),
+                    },
+                },
+                Face {
+                    bounds: Bounds {
+                        x: (9..13),
+                        y: (5..9),
+                    },
+                },
+                Face {
+                    bounds: Bounds {
+                        x: (13..17),
+                        y: (9..13),
+                    },
+                },
+                Face {
+                    bounds: Bounds {
+                        x: (9..13),
+                        y: (9..13),
+                    },
+                },
+            ]),
+        }
+    }
+
+    fn new_input() -> Self {
+        Self {
+            faces: Vec::from([
+                Face {
+                    bounds: Bounds {
+                        x: (51..101),
+                        y: (1..51),
+                    },
+                },
+                Face {
+                    bounds: Bounds {
+                        x: (1..51),
+                        y: (151..201),
+                    },
+                },
+                Face {
+                    bounds: Bounds {
+                        x: (1..51),
+                        y: (101..151),
+                    },
+                },
+                Face {
+                    bounds: Bounds {
+                        x: (51..101),
+                        y: (51..101),
+                    },
+                },
+                Face {
+                    bounds: Bounds {
+                        x: (101..151),
+                        y: (1..51),
+                    },
+                },
+                Face {
+                    bounds: Bounds {
+                        x: (51..101),
+                        y: (101..151),
+                    },
+                },
+            ]),
+        }
+    }
+
+    fn cube_index(&self, pos: &Pos) -> Option<u8> {
+        self.faces
+            .iter()
+            .position(|f| f.bounds.contains(pos))
+            .and_then(|index| Some((index + 1) as u8))
+    }
+
+    fn get_face(&self, number: u8) -> &Face {
+        self.faces.get(number as usize - 1).unwrap()
+    }
+
+    fn from(&self, from: &Pos, direction: &Direction) -> (Pos, Direction) {
+        println!("Searching face for {:?}", from);
+        let face_nb = self.cube_index(from).unwrap();
+        let face = self.get_face(face_nb);
+        let position = match direction {
+            Direction::Bottom | Direction::Top => from.x - face.bounds.x.start,
+            Direction::Left | Direction::Right => from.y - face.bounds.y.start,
+        };
+        let (destination_face_nb, new_direction) = match direction {
+            Direction::Bottom => Self::normal_down(face_nb),
+            Direction::Left => Self::normal_left(face_nb),
+            Direction::Right => Self::normal_right(face_nb),
+            Direction::Top => Self::normal_up(face_nb),
+        };
+        let destination_face = self.get_face(destination_face_nb);
+        println!(
+            "Moving from cube {:?} facing {:?} to cube {:?} facing {:?}",
+            face_nb, direction, destination_face_nb, new_direction
+        );
+        (
+            destination_face.at(position, direction, &new_direction),
+            new_direction,
+        )
+    }
+
+    fn normal_up(from: u8) -> (u8, Direction) {
+        match from {
+            1 => (2, Direction::Right),
+            2 => (3, Direction::Top),
+            3 => (4, Direction::Right),
+            4 => (1, Direction::Top),
+            5 => (2, Direction::Top),
+            6 => (4, Direction::Top),
+            _ => panic!("Invalid index"),
+        }
+    }
+
+    fn normal_down(from: u8) -> (u8, Direction) {
+        match from {
+            1 => (4, Direction::Bottom),
+            2 => (5, Direction::Bottom),
+            3 => (2, Direction::Bottom),
+            4 => (6, Direction::Bottom),
+            5 => (4, Direction::Left),
+            6 => (2, Direction::Left),
+            _ => panic!("Invalid index"),
+        }
+    }
+
+    fn normal_left(from: u8) -> (u8, Direction) {
+        match from {
+            1 => (3, Direction::Right),
+            2 => (1, Direction::Bottom),
+            3 => (1, Direction::Right),
+            4 => (3, Direction::Bottom),
+            5 => (1, Direction::Left),
+            6 => (3, Direction::Left),
+            _ => panic!("Invalid index"),
+        }
+    }
+
+    fn normal_right(from: u8) -> (u8, Direction) {
+        match from {
+            1 => (5, Direction::Right),
+            2 => (6, Direction::Top),
+            3 => (6, Direction::Right),
+            4 => (5, Direction::Top),
+            5 => (6, Direction::Left),
+            6 => (5, Direction::Left),
+            _ => panic!("Invalid index"),
+        }
+    }
+}
+
 struct Blocked {}
 
 struct World {
     cells: Vec<Cell>,
+    cube: Cube,
     width: usize,
     height: usize,
     player_dir: Direction,
@@ -80,6 +303,7 @@ impl World {
             cells: final_cells,
             player_dir: Direction::Right,
             player_pos: Pos { x: first_x, y: 1 },
+            cube: Cube::new_input(),
         }
     }
 
@@ -88,50 +312,6 @@ impl World {
             .or_else(|| panic!("Tried to get {}, {}", x, y))
             .unwrap()
             + x
-    }
-
-    fn first_in_line(&self, y: usize) -> Pos {
-        let x = self.cells[y * self.width..(y + 1) * self.width]
-            .iter()
-            .position(|c| c != &Cell::Void)
-            .unwrap();
-        Pos { x, y }
-    }
-
-    fn last_in_line(&self, y: usize) -> Pos {
-        let x = self.cells[y * self.width..(y + 1) * self.width]
-            .iter()
-            .rev()
-            .position(|c| c != &Cell::Void)
-            .unwrap();
-        Pos {
-            x: self.width - 1 - x,
-            y,
-        }
-    }
-
-    fn first_in_column(&self, x: usize) -> Pos {
-        let mut y = 1;
-        loop {
-            let pos = Pos { x, y };
-            match self.at(&pos) {
-                Some(Cell::Open | Cell::Wall) => return pos,
-                _ => {}
-            }
-            y += 1;
-        }
-    }
-
-    fn last_in_column(&self, x: usize) -> Pos {
-        let mut y = self.height;
-        loop {
-            let pos = Pos { x, y };
-            match self.at(&pos) {
-                Some(Cell::Open | Cell::Wall) => return pos,
-                _ => {}
-            }
-            y -= 1;
-        }
     }
 
     fn at(&self, Pos { x, y }: &Pos) -> Option<&Cell> {
@@ -144,26 +324,30 @@ impl World {
                 self.player_pos = destination;
                 Ok(())
             }
-            Some(Cell::Void) => match self.player_dir {
-                Direction::Right => self.move_to(self.first_in_line(destination.y)),
-                Direction::Left => self.move_to(self.last_in_line(destination.y)),
-                Direction::Bottom => self.move_to(self.first_in_column(destination.x)),
-                Direction::Top => self.move_to(self.last_in_column(destination.x)),
-            },
+            Some(Cell::Void) => {
+                let (new_dest, new_dir) = self.cube.from(&self.player_pos, &self.player_dir);
+                assert_ne!(new_dest, destination);
+                self.move_to(new_dest).and_then(|_| {
+                    self.player_dir = new_dir;
+                    println!("Now at {:?} facing {:?}", self.player_pos, self.player_dir);
+                    Ok(())
+                })
+            }
             Some(Cell::Wall) => Err(Blocked {}),
             _ => panic!("Unexpected destination"),
         }
     }
 
     fn move_player(&mut self, distance: usize) {
-        let offset = match self.player_dir {
-            Direction::Bottom => (0, 1),
-            Direction::Left => (-1, 0),
-            Direction::Top => (0, -1),
-            Direction::Right => (1, 0),
-        };
         for _ in 0..distance {
+            let offset = match self.player_dir {
+                Direction::Bottom => (0, 1),
+                Direction::Left => (-1, 0),
+                Direction::Top => (0, -1),
+                Direction::Right => (1, 0),
+            };
             let destination = &self.player_pos + &offset;
+            println!("Moving to {:?}", destination);
             match self.move_to(destination) {
                 Ok(_) => {}
                 Err(_) => break,
